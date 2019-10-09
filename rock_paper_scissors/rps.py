ddd08d0c8dc90485644045cf3b594dc43a5dfe05
Maxime Salomon <maxime@croissant.io>
2019-10-09 18:03:15 +0200

    rock_paper_scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,19 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  array = []
+  list = ['rock', 'paper', 'scissors']  
+  def rps(n, results):
+    if n == 0:
+      array.append(results)
+    else:
+      for i in range(len(list)):
+        rps((n - 1), results + [list[i]])
 
+  rps(n,[])
+  return array
+
+# print(rock_paper_scissors(2))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
